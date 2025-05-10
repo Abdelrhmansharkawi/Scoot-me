292d76fb76efe17218805065002bb1929c731a53
Abdelrhmansharkawi <abdelrhmansharkawi@gmail.com>
2025-05-10 10:11:40 +0300

    h

--- Front/src/BookRide.jsx
+++ Front/src/BookRide.jsx
@@ -90,7 +90,7 @@ const res = await axios.get(`${API_URL}/api/scooter`, {
       {/* Log Out Button */}
       <button
         onClick={handleLogOut}
-        className="absolute top-10 right-8 bg-orange-500 hover:bg-orange-600 text-white px-4 py-2 rounded-full font-semibold transition"
+        className="absolute top-4 right-4 bg-orange-500 hover:bg-orange-600 text-white px-4 py-2 rounded-full font-semibold transition"
       >
         Log Out
       </button>
