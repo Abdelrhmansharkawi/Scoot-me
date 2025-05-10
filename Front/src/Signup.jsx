f9d19358844c390acb90c12e93dcfef1c0c6806e
Abdelrhmansharkawi <abdelrhmansharkawi@gmail.com>
2025-05-10 08:08:53 +0300

    Fixed API request and template literal syntax

--- Front/src/Signup.jsx
+++ Front/src/Signup.jsx
@@ -4,7 +4,7 @@ import { FaEye, FaEyeSlash } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import axios from "axios";
 
-const API_URL = "https://scoot-me-production.up.railway.app/";
+const API_URL = "https://scoot-me-production.up.railway.app";
 
 function Signup() {
   const [firstName, setFirstName] = useState("");
